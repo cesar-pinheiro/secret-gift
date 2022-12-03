ce5da3106cf03afc611f7a47bd66a6a491c5c5d2
César A Pinheiro <cesar.a.pinheiro@hotmail.com>
2022-12-03 18:15:58 -0300

    ajuste responsividade do layout

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout(props: LayoutProps): JSX.Element {
         <div className={`
             absolute flex flex-col
             top-0 bottom-0 left-0 right-0 m-auto p-0 max-h-[40rem] max-w-[22rem]
-            md:max-h-[44rem] md:max-w-[32rem]
+            md:max-h-[32rem] md:max-w-[22rem]
             bg-[#557153] shadow-2xl rounded-2xl
         `}>
             <Header />
